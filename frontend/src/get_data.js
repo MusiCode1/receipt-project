835bc36cd3dc2946a8f56d41a70e9d18ac66cf58
MusiCode1 <MusiCode3@gmail.com>
2022-06-24 00:17:21 +0300

    הוספת fetch

--- frontend/src/get_data.js
+++ frontend/src/get_data.js
@@ -1,3 +1,26 @@
-const base_url = ""
 
-fetch()
+const base_url = import.meta.env.VITE_BASE_URL;
+
+export async function fn(resource) {
+
+    const res = await fetch(`${base_url}api/${resource}`);
+
+    return  res;
+
+}
+
+async function create(params) {
+    
+}
+
+async function read(resource) {
+    
+}
+
+async function update(params) {
+    
+}
+
+async function remove(params) {
+    
+}
