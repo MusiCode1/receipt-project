1eedc00ae2a2b7d0fa456e0c7f772f9134156eb6
MusiCode1 <MusiCode3@gmail.com>
2022-07-08 01:39:49 +0300

    ורסל

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
