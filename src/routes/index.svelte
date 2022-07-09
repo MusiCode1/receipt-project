e923ea066b1084bf23c99c0116faf3c7ad28f601
MusiCode1 <MusiCode3@gmail.com>
2022-07-10 00:58:57 +0300

    please go to the providers page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1 class="my-4">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="my-4">Hallo. please go to the providers page</h1>
+<p>It's here: <a href="/providers">Providers page</a></p>
