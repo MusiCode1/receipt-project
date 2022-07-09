cb4ef2f51b2fac7d9cc2b15b1f1d19a390b0358f
MusiCode1 <MusiCode3@gmail.com>
2022-07-10 01:41:12 +0300

    הסרת הקונטיינר על הנב-בר

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -20,29 +20,35 @@
 
 <Styles />
 
+<Navbar color="dark" dark expand="md">
+	<NavbarBrand href="/">Magen</NavbarBrand>
+
+	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+
+	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+		<Nav class="ms-auto" navbar>
+			<NavItem>
+				<NavLink href="/receipt">Receipt</NavLink>
+			</NavItem>
+
+			<NavItem>
+				<NavLink href="/providers">Providers</NavLink>
+			</NavItem>
+		</Nav>
+	</Collapse>
+</Navbar>
 <Container class="text-center">
-	<Navbar color="dark" dark expand="md">
-		<NavbarBrand href="/">Magen</NavbarBrand>
-
-		<NavbarToggler on:click={() => (isOpen = !isOpen)} />
-
-		<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-			<Nav class="ms-auto" navbar>
-				<NavItem>
-                    <NavLink href="/receipt">Receipt</NavLink>
-				</NavItem>
-
-				<NavItem>
-                    <NavLink href="/providers">Providers</NavLink>
-				</NavItem>
-			</Nav>
-		</Collapse>
-	</Navbar>
-    <slot></slot>
+	<slot />
 </Container>
 
 <style>
 	:global(body) {
 		background-color: #d7d7d7;
+	}
+
+	:global(.navbar) {
+		position: sticky;
+		top: 0;
+		z-index: 1;
 	}
 </style>
